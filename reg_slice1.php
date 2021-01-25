6b3fc477e905a1ee87b55d2b37a27cc8b49dbe7d
Laith Qaid <74902405+MAnMan711@users.noreply.github.com>
2021-01-25 21:01:11 +0800

    Add files via upload

---
+++ reg_slice1.php
@@ -0,0 +1,168 @@
+<?php
+session_start();
+ob_start();
+?>
+
+<html>
+<head>
+
+<title>Register</title>
+ <!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+<!--===============================================================================================-->  
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="utf-8">
+<link rel="stylesheet" href="css/style-footer.css">
+<link href="css/style1.css" rel="stylesheet">
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+<link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" media="all">
+<link href="css/style-body.css" rel="stylesheet" type="text/css" media="all"/>
+      
+<script src="js/slice1.js"></script>
+</head>
+
+<?php
+	
+      $var=$_GET['var'];
+	$_SESSION['a'][5]=$_GET['var'];	
+      $_SESSION['layer1']=$_GET['var'];
+
+?>
+
+<body>
+<!--Main Header-->
+<nav class="navbar navbar-default">
+         <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+				<a class="navbar-brand" style="color:#fff">Graphical Password Authentication System</a>
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"
+                          aria-expanded="false">
+                          <span class="sr-only">Toggle navigation</span>
+                          <span class="icon-bar"></span>
+                          <span class="icon-bar"></span>
+                          <span class="icon-bar"></span>
+                    </button>
+              </div>
+              <!-- Collect the nav links, forms, and other content for toggling -->
+              <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav pull-right">
+                          <li class="active">
+                                <a href="../index.html">Home</a>
+                          </li>
+                          <li>
+                                <a href="../about.html">About Us</a>
+                          </li>
+                       
+                          <li>
+                                <a href="../contact.html">Contact Us</a>
+                          </li>                                             
+                    </ul>
+              </div>
+              <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+    <!--End Main Header -->
+
+<body style="background-color: #666666;">
+	
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+				<div class="login100-form ">
+					<span class="login100-form-title p-b-43">Set Graphical password for Your Account</span>
+					<p class="account1">Following is the 1st image you selected.</p>
+					<img class="im" src=<?php echo $var; ?> onload="changeIt(this)" height="100" width="100">
+				<p class="account">Select the part you want to set as the image part.</p>
+				<center><div class="test" id="test"></div></center>
+				</div>
+
+				<div class="login100-more" style="background-image: url('images/bg-01.jpg');">
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+<!--footer-main-->
+ <footer class="footer-distributed" style="height:300px">
+  <div id="page-content">
+    <div class="container text-center">
+      
+    </div>
+  
+  <div class="footer-right">
+        <div>
+          <i class="fa fa-map-marker" style="color:#fff"></i>
+          <p style="color:#fff" ><span>Garden@ One south  Serdang </span> Selangor, Country</p>
+        </div>
+</div>
+
+      <div class="footer-icons pull-right">
+                                      
+                        <a href="https://www.facebook.com/" target="_blank"><i class="fa fa-facebook"></i></a>
+                        <a href="https://www.linkedin.com/" target="_blank"><i class="fa fa-linkedin"></i></a>
+                        <a href="https://github.com/" target="_blank"><i class="fa fa-github"></i></a>
+                                      
+                  </div>
+</div>
+      <div class="footer-icons pull-right">
+                                      
+                        <a href="https://www.facebook.com/" target="_blank"><i class="fa fa-facebook"></i></a>
+                        <a href="https://twitter.com/?lang=en" target="_blank"><i class="fa fa-twitter"></i></a>
+                        <a href="https://www.linkedin.com/" target="_blank"><i class="fa fa-linkedin"></i></a>
+                        <a href="https://github.com/" target="_blank"><i class="fa fa-github"></i></a>
+                                      
+                  </div>
+				  </div>
+</div>
+
+	
+ 
+  </footer>
+<!--===============================================================================================-->
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/daterangepicker/moment.min.js"></script>
+	<script src="vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->   
+
+<script src="plugins/jquery.js"></script>
+<script src="plugins/bootstrap.min.js"></script>
+<script src="plugins/bootstrap-select.min.js"></script>
+
+<script src="plugins/validate.js"></script>
+<script src="plugins/wow.js"></script>
+<script src="plugins/jquery-ui.js"></script>
+<script src="js/script.js"></script>
+</body>
+
+</html>
+
